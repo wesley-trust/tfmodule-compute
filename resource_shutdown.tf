5b5e41b5522d6c75981e9a8697b45efe9602c534
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-06-12 14:07:29 +0100

    Correct null reference

--- resource_shutdown.tf
+++ resource_shutdown.tf
@@ -1,5 +1,5 @@
 resource "azurerm_dev_test_global_vm_shutdown_schedule" "shutdown_schedule" {
-  count              = var.resource_shutdown_enabled == true ? var.resource_instance_count : null
+  count              = var.resource_shutdown_enabled == true ? var.resource_instance_count : 0
   virtual_machine_id = module.virtual_machine[count.index].id
   location           = module.resource_group.location
   enabled            = true
