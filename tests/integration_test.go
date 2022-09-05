c675fb063dd1b828c2d5140ebacb05305fa5e655
wesley-trust <wesley-trust@users.noreply.github.com>
2022-09-05 19:08:53 +0100

    Add tests

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -56,3 +56,48 @@ func TestApplyMulti_Instance_Region_Disks_NI_LB_Shutdown(t *testing.T) {
 	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
 	terraform.InitAndApply(t, terraformOptions)
 }
+
+func TestApplyMulti_Instance_Single_Region_Scale_Set(t *testing.T) {
+	t.Parallel()
+
+	// Root folder where Terraform files should be (relative to the test folder)
+	rootFolder := "../"
+
+	// Path to Terraform example files being tested (relative to the root folder)
+	terraformFolderRelativeToRoot := "./examples/"
+
+	// Copy the terraform folder to a temp folder to prevent conflicts from parallel runs
+	tempTestFolder := test_structure.CopyTerraformFolderToTemp(t, rootFolder, terraformFolderRelativeToRoot)
+
+	// Generate a random deployment name for the test to prevent a naming conflict
+	uniqueID := random.UniqueId()
+	testREF := "Multi_Instance_Single_Region_Scale_Set"
+	serviceDeployment := testREF + "-" + uniqueID
+
+	// Define variables
+	locations := []string{"UK South"}
+
+	// Enable retryable error
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+
+		// The path to where the Terraform code is located
+		TerraformDir: tempTestFolder,
+
+		// Variables to pass to the Terraform code using -var options
+		Vars: map[string]interface{}{
+			"service_deployment":      serviceDeployment,
+			"resource_instance_count": 2,
+			"service_location":        locations,
+			"provision_scale_set":     true,
+		},
+	})
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.DestroyE(t, terraformOptions)
+
+	// At the end of the test, run `terraform destroy` again, in case failures leave orphaned resources
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
+	terraform.InitAndApply(t, terraformOptions)
+}
