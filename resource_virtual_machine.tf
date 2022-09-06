c5fcd2b9f61e600d089515b5d84e3b5f692ae231
wesley-trust <wesley-trust@users.noreply.github.com>
2022-09-06 11:40:22 +0100

    Bump module version

--- resource_virtual_machine.tf
+++ resource_virtual_machine.tf
@@ -19,7 +19,7 @@ module "virtual_machine" {
   depends_on = [
     module.network_interfaces
   ]
-  source                    = "github.com/wesley-trust/tfsubmodule-virtual_machine?ref=v1.1-virtual_machine"
+  source                    = "github.com/wesley-trust/tfsubmodule-virtual_machine?ref=v1.2-virtual_machine"
   count                     = var.provision_scale_set != true ? var.resource_instance_count : 0
   name                      = "${local.resource_name}${format("%02d", count.index + 1)}-vm"
   source_image_id           = var.resource_image != null ? data.azurerm_image.search[0].id : null
