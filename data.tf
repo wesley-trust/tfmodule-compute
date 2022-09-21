fb5e53f60ab71b1371e6007707f97c95bf38dab4
wesley-trust <wesley-trust@users.noreply.github.com>
2022-09-21 11:23:53 +0100

    Update image code

--- data.tf
+++ data.tf
@@ -1,14 +1,20 @@
 data "azurerm_images" "search" {
   count = var.resource_image != null ? 1 : 0
 
-  images {
-    name = var.resource_image
-    tags = {
-      version     = "latest"
-      environment = var.service_environment
-      deployment  = var.service_deployment
-    }
+  resource_group_name = var.resource_image_group
+
+  tags_filter = {
+    name_prefix = var.resource_image
+    version     = "latest"
+    environment = var.service_environment
+    deployment  = var.service_deployment
   }
+}
+
+data "azurerm_image" "select" {
+  count = var.resource_image != null ? 1 : 0
+
+  name = data.azurerm_images.search[0].images[0].name
 
   resource_group_name = var.resource_image_group
 }
